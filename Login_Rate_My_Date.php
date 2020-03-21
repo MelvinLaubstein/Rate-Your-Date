94b101e14a9fb8ec2582b0d10c843cb1fd6b0bc8
mdlaubstein <mdlaubstein@oakland.edu>
2020-03-21 15:37:40 -0400

    Login now differentiates successfully. Signup and Login forms changed slightly.

--- Login_Rate_My_Date.php
+++ Login_Rate_My_Date.php
@@ -9,7 +9,7 @@ session_start();
 	$username = $_POST["username"];
 	$password = $_POST["password"];
 	$_SESSION['username'] = $username;
-
+if (isset($_POST['admin'])) {
 	$sql = "SELECT * from administrator WHERE (administrator_username='$username' AND administrator_password='$password')";
 	$check = mysqli_query($conn, $sql);
 	if (mysqli_num_rows($check) >= 1) {
@@ -17,5 +17,15 @@ session_start();
 	} else {
 		echo "<script> alert('Login Failed!'); window.location.href='Login_Rate_My_Date.html' </script>"; 
 	}
-	
+}
+
+if (isset($_POST['rater'])) {
+	$sql = "SELECT * from rater WHERE (rater_username='$username' AND rater_password='$password')";
+	$check = mysqli_query($conn, $sql);
+	if (mysqli_num_rows($check) >= 1) {
+		header("location: Homepage_Rate_My_Date.php");
+	} else {
+		echo "<script> alert('Login Failed!'); window.location.href='Login_Rate_My_Date.html' </script>"; 
+	}
+}
 ?>
