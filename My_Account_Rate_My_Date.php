92680b437b3dc5b095176ae0c9e2a37996ddb0c7
mdlaubstein <mdlaubstein@oakland.edu>
2020-04-09 21:07:41 -0400

    Added administrator functionality for My Account.

--- My_Account_Rate_My_Date.php
+++ My_Account_Rate_My_Date.php
@@ -43,6 +43,7 @@
 					$loggedInUser = $_SESSION['username'];
 					
 					$checkRater = mysqli_query($conn, "SELECT * from RATER where RATER_USERNAME='$loggedInUser'");
+					$checkAdministrator = mysqli_query($conn, "SELECT * from ADMINISTRATOR where ADMINISTRATOR_USERNAME='$loggedInUser'");
 					if (mysqli_num_rows($checkRater) > 0) {
 						while($data = mysqli_fetch_assoc($checkRater)) {
 							echo "<b> Your Profile Picture </b> <br>";
@@ -80,6 +81,51 @@
 								if (($_POST['oldPassword'] == $data['RATER_PASSWORD'])) {
 									$newPassword = $_POST['newPassword'];
 								mysqli_query($conn, "UPDATE RATER SET RATER_PASSWORD='$newPassword' where RATER_USERNAME='$loggedInUser'");
+								echo "<script> alert('Your password has been updated!'); </script>";
+								echo "<script> window.location.href='My_Account_Rate_My_Date.php'</script>";
+							} else {
+								echo "<script> alert('Your input for your old (Current) password is incorrect!') </script>";
+							}
+						}
+								echo "</form>";
+							}
+						} else if (mysqli_num_rows($checkAdministrator) > 0) {
+							while($data = mysqli_fetch_assoc($checkAdministrator)) {
+							echo "<b> Your Profile Picture </b> <br>";
+							echo '<img width="200px" height="200px" src="data:image/png;base64,' . $data['ADMINISTRATOR_PROFILE_PICTURE'] . '" />';
+							echo "<br>";
+							echo "<input id='picture1' name='picture1' type='file'>";
+							echo ("<input type='submit' name='picturetureSubmit1' id='picturetureSubmit1' value='Change Profile pictureture (Select a File First)'>");
+							echo "<br><br><br>";
+							echo ("Username: " . $loggedInUser);
+							echo "<br><br>";
+							echo "Old Password: " . "<input type='password' name='oldPassword1' id='oldPassword1'>" . " " . " New Password: " . "<input type='password' id='newPassword1' name='newPassword1'>" . " " . "<input type='submit' id='passwordSubmit1' name='passwordSubmit1' value='Change Password'>";
+							echo "<br><br>";
+							echo "Current Email Address: " . $data['ADMINISTRATOR_EMAIL_ADDRESS'] . " <br><br> " . " New Email Address: " . "<input type='email' name='newEmail1' id='newEmail1'>" . "<input type='submit' id='emailSubmit1' name='emailSubmit1' value='Change Email Address'>";
+						
+							if (isset($_POST['picturetureSubmit1'])) {
+							  if (isset($_FILES['picture1']['name'])) {
+								$picture1 = base64_encode(file_get_contents($_FILES['picture1']['tmp_name']));
+								mysqli_query($conn, "UPDATE ADMINISTRATOR SET ADMINISTRATOR_PROFILE_PICTURE='$picture1' where ADMINISTRATOR_USERNAME='$loggedInUser'");
+								echo "<script> window.location.href='My_Account_Rate_My_Date.php' </script>"; 	
+							  } else {
+
+							  }
+							} else {
+
+							}
+						
+							if (isset($_POST['emailSubmit1'])) {
+								$newEmail1 = $_POST['newEmail1'];
+								mysqli_query($conn, "UPDATE ADMINISTRATOR SET ADMINISTRATOR_EMAIL_ADDRESS='$newEmail1' where ADMINISTRATOR_USERNAME='$loggedInUser'");
+								echo "<script> alert('Your email address was successfully updated!'); </script>";
+								echo "<script> window.location.href='My_Account_Rate_My_Date.php' </script>"; 				
+							}
+							
+							if (isset($_POST['passwordSubmit1'])) {
+								if (($_POST['oldPassword1'] == $data['ADMINISTRATOR_PASSWORD'])) {
+									$newPassword1 = $_POST['newPassword1'];
+								mysqli_query($conn, "UPDATE ADMINISTRATOR SET ADMINISTRATOR_PASSWORD='$newPassword1' where ADMINISTRATOR_USERNAME='$loggedInUser'");
 								echo "<script> alert('Your password has been updated!'); </script>";
 								echo "<script> window.location.href='My_Account_Rate_My_Date.php'</script>";
 							} else {
